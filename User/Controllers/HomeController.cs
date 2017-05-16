1dd4be35b88c0f1c7f58198e51548f51cf6aa1c3
hoyuo <psh9907@gmail.com>
2017-05-16 22:57:05 +0900

    Init
    기본적인 로그인 처리

---
+++ User/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace User.Controllers
+{
+	public class HomeController : Controller
+	{
+		// GET: Home
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Page1()
+		{
+			if (Request.IsAuthenticated)
+			{
+				return View();
+			}
+			return RedirectToAction("Index");
+
+		}
+
+		public ActionResult Page2()
+		{
+			return View();
+		}
+
+		public ActionResult Page3()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Login()
+		{
+			FormsAuthentication.SetAuthCookie("TEST", false);
+			return RedirectToAction("Page1", "Home");
+		}
+
+		public ActionResult Logout()
+		{
+			FormsAuthentication.SignOut();
+			return RedirectToAction("Index", "Home");
+		}
+	}
+}
